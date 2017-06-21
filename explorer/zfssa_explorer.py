2903d15dd5a338ff2fdc45e15f23281df9f0b636
Aldo Sotolongo <aldenso@gmail.com>
2017-06-20 23:26:38 -0400

    first commit for explorer script

---
+++ explorer/zfssa_explorer.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @CreateTime: Jun 20, 2017 3:51 PM
+# @Author: Aldo Sotolongo
+# @Contact: aldenso@gmail.com
+# @Last Modified By: Aldo Sotolongo
+# @Last Modified Time: Jun 20, 2017 7:01 PM
+# @Description: Modify Here, Please
+
+from __future__ import print_function, division
+# import re
+# import json
+# import csv
+from datetime import datetime
+# import ast
+import argparse
+# import logging
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import requests
+#from requests.exceptions import HTTPError, ConnectionError
+from urllib3.exceptions import InsecureRequestWarning
+import yaml
+
+# to disable warning
+# InsecureRequestWarning: Unverified HTTPS request is being made.
+# Adding certificate verification is strongly advised. See:
+# https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
+requests.urllib3.disable_warnings(InsecureRequestWarning)
+
+START = datetime.now()
+ZFSURL = ""  # API URL (https://example:215/api)
+ZAUTH = ()   # API Authentication tuple (username, password)
+HEADER = {"Content-Type": "application/json"}
+LOGFILE = "explorer_output.log"
+
+
+def create_parser():
+    """Get Arguments"""
+    parser = argparse.ArgumentParser(
+        description="Script to get ZFS Storage Appliance info")
+    parser.add_argument(
+        "-s", "--server", type=str, help="Server config file (YAML)", required=True)
+    # parser.add_argument(
+    #     "-p", "--progress", action="store_true", help="progress bar and logging to file",
+    #     required=False)
+    return parser
+
+
+def read_yaml_file(configfile):
+    """Read config file and return credentials in json."""
+    config = {}
+    try:
+        config = yaml.load(file(configfile, 'r'))
+    except yaml.YAMLError as error:
+        print("Error in configuration file: {}").format(error)
+    return config
+
+
+def fetch(url, timeout, datatype):
+    """Fetch data from zfs api"""
+    req = requests.get(url, timeout=timeout, auth=ZAUTH, verify=False, headers=HEADER)
+    data = req.json()
+    return data, datatype
+
+def printdata(data, datatype):
+    """Print data in a human readable way"""
+    if datatype == "version":
+        print("#" * 100)
+        print("ZFS Storage Appliance version")
+        print("#" * 100)
+        print("{:20} {:30}".format("version", "ak_version"))
+        print("{:20} {:30}".format(data['version']['product'], data['version']['ak_version']))
+    elif datatype == "datalinks":
+        print("#" * 100)
+        print("datalink info")
+        print("#" * 100)
+        print("{:10} {:20} {:8}".format("class", "label", "speed"))
+        for dlink in data['datalinks']:
+            print("{:10} {:20} {:8}".format(dlink['class'], dlink['label'], dlink['speed']))
+    elif datatype == "devices":
+        print("#" * 100)
+        print("devices info")
+        print("#" * 100)
+        print("{:10} {:20} {:32} {:8} {:16} {:6}".format("active", "duplex", "mac", "media",
+                                                         "speed", "up"))
+        for dev in data['devices']:
+            print("{:10} {:10} {:32} {:10} {:8} {:5}".format(dev['active'], dev['duplex'],
+                                                             dev['factory_mac'], dev['media'],
+                                                             dev['speed'], dev['up']))
+    elif datatype == "interfaces":
+        print("#" * 100)
+        print("interfaces info")
+        print("#" * 100)
+        print("{:10} {:15} {:8} {:8} {:40} {:5}".format("class", "label", "admin", "state",
+                                                        "v4addrs", "enable"))
+        for dev in data['interfaces']:
+            print("{:10} {:15} {:8} {:8} {:40} {:5}".format(dev['class'], dev['label'],
+                                                            dev['admin'], dev['state'],
+                                                            dev['v4addrs'], dev['enable']))
+
+
+def main(args):
+    configfile = args.server
+    config = read_yaml_file(configfile)
+    global ZFSURL, ZAUTH
+    ZFSURL = "https://{}:215/api".format(config['ip'])
+    ZAUTH = (config['username'], config['password'])
+    group1 = [("{}/system/v1/version".format(ZFSURL), "version"),
+              #("{}/hardware/v1/cluster".format(ZFSURL), "cluster")
+              ("{}/network/v1/datalinks".format(ZFSURL), "datalinks"),
+              ("{}/network/v1/devices".format(ZFSURL), "devices"),
+              ("{}/network/v1/interfaces".format(ZFSURL), "interfaces")]
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = {}
+        for i in group1:
+            url = i[0]
+            future = executor.submit(fetch, url, 100, i[1])
+            futures[future] = url
+
+        for future in as_completed(futures):
+            url = futures[future]
+            try:
+                data, datatype = future.result()
+            except Exception as exc:
+                print(exc)
+            else:
+                printdata(data, datatype)
+
+
+if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+    # if args.progress:
+    #     try:
+    #         from progress.bar import Bar
+    #     except ImportError as err:
+    #         print("You need to install progress: pip install progress - Error: {}".format(err))
+    #         exit(1)
+    main(args)
