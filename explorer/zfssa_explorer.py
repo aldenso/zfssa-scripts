41cb286bcfb3681669c8df26ee13b8b355f7fc39
Aldo Sotolongo <aldenso@gmail.com>
2017-12-19 13:16:16 -0400

    changed human size format

--- explorer/zfssa_explorer.py
+++ explorer/zfssa_explorer.py
@@ -59,8 +59,8 @@ def response_size(nbytes):
     while nbytes >= 1024 and i < len(suffixes)-1:
         nbytes /= 1024.
         i += 1
-    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
-    return '%s %s' % (f, suffixes[i])
+    f = ('{:.2f}'.format(nbytes)).rstrip('0').rstrip('.')
+    return '{} {}'.format(f, suffixes[i])
 
 
 def exists(data, key):
