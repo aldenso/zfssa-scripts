b3c0409ac81f5735e04287edc76ef9dabd0ae67c
Aldo Sotolongo <aldenso@gmail.com>
2018-01-29 10:10:22 -0400

    added args for explorer scheduler

--- explorer/explorer_scheduler.py
+++ explorer/explorer_scheduler.py
@@ -4,29 +4,42 @@
 # Contact: aldenso@gmail.com
 # Description: Schedule automatic explorer of ZFS Storage Appliance.
 
-from __future__ import print_function, division
+from __future__ import print_function
 import os
+import argparse
 import time
 import schedule
 import zfssa_explorer
-#from progressbar import ProgressBar, AdaptiveETA, Bar, Percentage
 
 
-def get_zfssalist():
+def create_parser():
+    """Get Arguments"""
+    parser = argparse.ArgumentParser(
+        description="Schedule zfssa explorers")
+    parser.add_argument("-d", "--directory", type=str,
+                        help="Directory to find Server config files (YAML)",
+                        required=True)
+    parser.add_argument("-t", "--time", action='append',
+                        help="24Hr time where the Job should be launched",
+                        required=True)
+    return parser
+
+def get_zfssalist(directory):
     """Return list of yml files in current directory"""
-    zfssalist = [file for file in os.listdir(os.getcwd()) if file.endswith('yml')]
-    if not zfssalist:
-        print('ZFSSA yaml not found in {}'.format(os.getcwd()))
+    files = [file for file in os.listdir(directory) if file.endswith('yml')]
+    if not files:
+        print('No yaml found in {}'.format(directory))
         exit(1)
+    zfssalist = [os.path.join(directory, file) for file in files]
     return zfssalist
 
 
 def launch_explorers(zfssalist):
     """Launch explorers from a zfsssa list"""
     for zfssa in zfssalist:
-        args = Namespace(server=zfssa, progress=True)
+        argsforexplorer = Namespace(server=zfssa, progress=True)
         print("Explorer for '{}' launched".format(zfssa.split('.')[0]))
-        zfssa_explorer.main(args)
+        zfssa_explorer.main(argsforexplorer)
 
 
 class Namespace:
@@ -36,8 +49,12 @@ class Namespace:
 
 
 if __name__ == "__main__":
-    zfssalist = get_zfssalist()
-    schedule.every().day.at("22:00").do(launch_explorers, zfssalist)
+    parser = create_parser()
+    args = parser.parse_args()
+    zfssalist = get_zfssalist(args.directory)
+    for schedtime in args.time:
+        print(schedtime, zfssalist)
+        schedule.every().day.at(schedtime).do(launch_explorers, zfssalist)
     while True:
         schedule.run_pending()
         time.sleep(1)
